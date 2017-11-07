f0a5ba30e55562c05a4e829a38511e2a22844b7b
aicekiller <1017634520@qq.com>
2017-11-07 09:53:36 +0800

    使用vue搭建系统后台

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <el-button>123</el-button>
+    <el-checkbox v-model="checked">备选项</el-checkbox>
+  </div>
+</template>
+
+<script>
+  import ElButton from '../../node_modules/element-ui/packages/button/src/button'
+  import ElCheckbox from '../../node_modules/element-ui/packages/checkbox/src/checkbox'
+  export default {
+    components: {
+      ElCheckbox,
+      ElButton
+    },
+    name: 'HelloWorld',
+    data () {
+      return {
+        msg: 'test',
+        checked: false
+      }
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  h1, h2 {
+    font-weight: normal;
+  }
+
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+
+  li {
+    display: inline-block;
+    margin: 0 10px;
+  }
+
+  a {
+    color: #42b983;
+  }
+</style>
