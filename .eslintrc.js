38f3748a7326921b1ea09015fff298c1c2d19ed2
aicekiller <1017634520@qq.com>
2017-11-07 21:32:32 +0800

    modify

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,10 @@
 // https://eslint.org/docs/user-guide/configuring
 
 module.exports = {
-  root: true,
-  parser: 'babel-eslint',
-  parserOptions: {
-    sourceType: 'module'
+  root: true,//ESLint 一旦发现配置文件中有 "root": true，它就会停止在父级目录中寻找。
+  parser: 'babel-eslint',//表明使用该 babel-eslint 模块作为解析器
+  parserOptions: { //设置解析器选项
+    sourceType: 'module' // 表明代码是ECMAScript 模块
   },
   env: {
     browser: true,
