913d750f0777660349d51af3753e3a58d7451a7e
aicekiller <1017634520@qq.com>
2017-11-09 15:27:30 +0800

    modify
    增加layout公共部分

--- src/components/Hamburger/index.vue
+++ src/components/Hamburger/index.vue
@@ -15,3 +15,35 @@
     </svg>
   </div>
 </template>
+
+<script>
+  export default {
+    name: 'Hamburger',
+    props: {
+      isActive: {
+        type: Boolean,
+        default: false
+      },
+      toggleClick: {
+        type: Function,
+        default: null
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+  .hamburger {
+    display: inline-block;
+    cursor: pointer;
+    width: 20px;
+    height: 20px;
+    transform: rotate(0deg);
+    transition: .38s;
+    transform-origin: 50% 50%;
+  }
+
+  .hamburger.is-active {
+    transform: rotate(90deg);
+  }
+</style>
