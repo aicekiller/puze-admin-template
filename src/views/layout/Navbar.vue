2738557df46286e75dd0dfefbea40d9e955cfa0c
aicekiller <1017634520@qq.com>
2017-11-10 17:48:09 +0800

    modify

---
+++ src/views/layout/Navbar.vue
@@ -0,0 +1,21 @@
+<template>
+  <el-menu class="navbar" mode="horizontal">
+
+  </el-menu>
+</template>
+
+<script>
+  import Levelbar from 'Levelbar.vue'
+  import Hamburger from '@/components/Hamburger'
+  export default {
+    components: {
+      Levelbar, Hamburger
+    },
+    computed:{
+
+    },
+    methods:{
+
+    }
+  }
+</script>
